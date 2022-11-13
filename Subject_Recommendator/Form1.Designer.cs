72d8123e3e337f491a8e2fddbdffb8ce6a1c7813
options3224@gmail.com <options3224@gmail.com>
2022-11-13 21:07:43 +0900

    메인 Form 완성(메뉴 제외)

--- Subject_Recommendator/Form1.Designer.cs
+++ Subject_Recommendator/Form1.Designer.cs
@@ -23,13 +23,206 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbTerm2 = new System.Windows.Forms.RadioButton();
+            this.rbTerm1 = new System.Windows.Forms.RadioButton();
+            this.rbTermAll = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.rbTerm2);
+            this.groupBox1.Controls.Add(this.rbTerm1);
+            this.groupBox1.Controls.Add(this.rbTermAll);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(548, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "교과목 추천 조건";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(389, 140);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(89, 19);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "불확실함";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(268, 140);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(59, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "없음";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(150, 140);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "있음";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(256, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "학년 이하의 과목만 추천합니다.";
+            // 
+            // rbTerm2
+            // 
+            this.rbTerm2.AutoSize = true;
+            this.rbTerm2.Location = new System.Drawing.Point(389, 100);
+            this.rbTerm2.Name = "rbTerm2";
+            this.rbTerm2.Size = new System.Drawing.Size(66, 19);
+            this.rbTerm2.TabIndex = 4;
+            this.rbTerm2.TabStop = true;
+            this.rbTerm2.Text = "2학기";
+            this.rbTerm2.UseVisualStyleBackColor = true;
+            // 
+            // rbTerm1
+            // 
+            this.rbTerm1.AutoSize = true;
+            this.rbTerm1.Location = new System.Drawing.Point(268, 100);
+            this.rbTerm1.Name = "rbTerm1";
+            this.rbTerm1.Size = new System.Drawing.Size(66, 19);
+            this.rbTerm1.TabIndex = 3;
+            this.rbTerm1.TabStop = true;
+            this.rbTerm1.Text = "1학기";
+            this.rbTerm1.UseVisualStyleBackColor = true;
+            // 
+            // rbTermAll
+            // 
+            this.rbTermAll.AutoSize = true;
+            this.rbTermAll.Location = new System.Drawing.Point(150, 100);
+            this.rbTermAll.Name = "rbTermAll";
+            this.rbTermAll.Size = new System.Drawing.Size(58, 19);
+            this.rbTermAll.TabIndex = 2;
+            this.rbTermAll.TabStop = true;
+            this.rbTermAll.Text = "전체";
+            this.rbTermAll.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "팀 과제: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "개설학기: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "학년: ";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(226, 287);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(144, 52);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "진단검사 시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(150, 57);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 25);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 375);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbTerm2;
+        private System.Windows.Forms.RadioButton rbTerm1;
+        private System.Windows.Forms.RadioButton rbTermAll;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
