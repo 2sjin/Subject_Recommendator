0197d537c2608afde73f03e5479f8751de3dbdd3
options3224@gmail.com <options3224@gmail.com>
2022-11-19 00:03:10 +0900

    엔티티 클래스(Subject, Question) 추가

---
+++ Subject_Recommendator/EntityClass/Question.cs
@@ -0,0 +1,7 @@
+﻿namespace Subject_Recommendator.EntityClass {
+    // 진단검사 문항 엔티티 클래스
+    internal class Question {
+        int Seq { get; set; }           // 문항 번호
+        string Content { get; set; }    // 문항 내용
+    }
+}
